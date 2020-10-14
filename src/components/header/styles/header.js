384f34b8995b67247c23bcf48dc96d30a463487b
phanendery <phanendery@gmail.com>
2020-10-14 15:17:16 -0700

    not much today

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,12 +1,21 @@
 import styled from "styled-components/macro";
 import { Link as ReactRouterLink } from "react-router-dom";
 
-export const Background = styled.div`
- 
-`;
+export const Background = styled.div``;
 
 export const Container = styled.div`
-
+  display: flex;
+  margin: 0 56px;
+  height: 64px;
+  padding: 18px 0;
+  justify-content: space-between;
+  align-items: center;
+  a {
+    display: flex;
+  }
+  @media (max-width: 1000px) {
+    margin: 0 30px;
+  }
 `;
 
 export const Logo = styled.img`

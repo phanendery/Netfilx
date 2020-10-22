7cf285d77fc5190255aa5406732f3ba8c1f26347
phanendery <phanendery@gmail.com>
2020-10-21 20:51:51 -0700

    designing next part

--- src/containers/header.js
+++ src/containers/header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Header } from "../components";
 import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
+//design structure for header?
 
 export function HeaderContainer({ children }) {
   return (

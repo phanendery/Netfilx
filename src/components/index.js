699270c1349b14d64d07599e1c4ba1f2db360b9b
phanendery <phanendery@gmail.com>
2020-10-27 13:53:43 -0700

    finished headers

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@
 //now i can just call from components
 //this is a component library
 export { default as Jumbotron } from "./jumbotron";
+export { default as Feature } from "./feature";
 export { default as Header } from "./header";
 export { default as Accordion } from "./accordion";
 export { default as Footer } from "./footer"; 

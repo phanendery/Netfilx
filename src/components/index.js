6e89abc89a3ca77a8407b4d55bdfe85ccc48f14e
phanendery <phanendery@gmail.com>
2020-11-02 15:10:09 -0800

    finished loader

--- src/components/index.js
+++ src/components/index.js
@@ -9,3 +9,4 @@ export { default as Footer } from "./footer";
 export { default as OptForm } from "./opt-form";
 export { default as Form } from "./form";
 export { default as Profiles } from "./profiles";
+export { default as Loading } from "./loading";
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
 import logo from "../logo.svg";
 import { SelectProfileContainer } from "./profiles";
+import { FirebaseContext } from "../context/firebase";
+import { Loading } from "../components";
 
 export function BrowseContainer({ slides }) {
   const [category, setCategory] = useState("series");
@@ -9,5 +11,20 @@ export function BrowseContainer({ slides }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [slideRows, setSlideRows] = useState([]);
 
-  return <SelectProfileContainer></SelectProfileContainer>;
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+    <div>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+    </div>
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }

a04df7016c67bde23442d2ffc48107b5a8fa383e
phanendery <phanendery@gmail.com>
2020-11-15 20:46:21 -0800

    future additions

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -3,6 +3,7 @@ import { LockBody, ReleaseBody, Spinner, Picture } from "./styles/loading";
 
 export default function Loading({ src, ...restProps }) {
   return (
+    //how to change spinner to load properly
     <Spinner {...restProps}>
       <LockBody />
       <Picture src={`/images/users/${src}.png`} data-testid="loading-picture" />
